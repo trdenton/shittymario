33ba59e1d4145c074f68ee4f105ee7a795039993
Troy Denton <trdenton@gmail.com>
2013-06-26 08:45:30 -0500

    Got square drawing working for pressing left shift
    also mapped freeform mode to spacebar instead of any key

--- leveledit.py
+++ leveledit.py
@@ -21,12 +21,18 @@ cbutts=[]	#control buttons - the ones to select current tile
 tbutts=[]	#tile buttons - the ones that make up the map
 currentTile = 0
 dragMode = 0
+dragBoxMode=0
+currentButton = (0,0)
+dragBoxMode_orig = (0,0)
+dragBoxMode_point = (5,5)
 #pygame.init()
 
 print "loading tiles..."
 tiles = spritesheet.loadTileSheet('SMB-Tiles.png',16,16,1,1)
 class Butt:
 	def __init__(self, master, tdex=0):
+		self.x=x
+		self.y=y
 		self.tiledex = tdex
 		self.setImage(tdex)
 		self.maxdex=len(tiles)
@@ -40,24 +46,45 @@ class Butt:
 
 	def setTile(self):
 		def ret():
+			global currentTile
+			#print "setting tiledex to " + str(currentTile)
 			self.tiledex = currentTile
 			self.setImage(self.tiledex)		
 		return ret
 class TButt(Butt):
-	def __init__(self, master, tdex=0):
+	def __init__(self, master, tdex=0,x=0,y=0):
 		Butt.__init__(self,master,tdex)
 		self.b.bind('<Motion>',self.enterB())
+		self.x=x
+		self.y=y
 
 	def enterB(self):
 		def ret(event):
 			global dragMode
+			global dragBoxMode_point
+			global currentButton
+			currentButton = (self.x,self.y)	
 			if (dragMode):
 				self.tiledex = currentTile
 				self.setImage(self.tiledex)		
 				#print "OVER!"
+			if (dragBoxMode):
+				print "DRAGBOX"
+				dragBoxMode_point = (self.x,self.y)
+				
+						
 		return ret
 
-
+def fillSquare( orig, end ):
+	global currentTile
+	global tbutts
+	for x in range(min(orig[0],end[0]),max(orig[0],end[0])+1):
+		for y in range(min(orig[1],end[1]),max(orig[1],end[1])+1):
+			
+			b = tbutts[x][y].setTile()
+			b()
+			#b.b.config(image=b.image)
+			#b.b.grid(row=b.x,column=b.y)	
 		
 
 def convertImage(im):
@@ -78,8 +105,17 @@ def selectTile(x):
 
 def keyPressHandler (event):
 	global dragMode
-	dragMode=1-dragMode
-	print "down"
+	global dragBoxMode
+	global dragBoxMode_orig
+	global dragBoxMode_point
+	global currentButton
+	if (event.keysym == "space"):
+		dragMode=1-dragMode
+	if (event.keysym == "Shift_L"):
+		dragBoxMode = 1- dragBoxMode
+		if (dragBoxMode == 1):
+			dragBoxMode_orig = currentButton
+	print event.keysym
 def mouseUp (event):
 	mouseIsDown=0
 	print "up"
@@ -103,7 +139,7 @@ root.title('Shitty Mario Level Editor')
 for x in range(30):
 	tbutts.append([])
 	for y in range(30):
-		b = TButt(topFrame,0)
+		b = TButt(topFrame,0,x,y)
 		tbutts[x].append(b)
 		tbutts[x][y].b.config(image=b.image)
 		tbutts[x][y].b.grid(row=x,column=y)
@@ -118,6 +154,15 @@ for x in range(len(tiles)):
 
 def task():
 	global currentTile
+	global dragBoxMode_orig
+	global dragBoxMode_point
+	global dragBoxMode
+	print dragBoxMode_orig
+	if (dragBoxMode):
+		fillSquare(dragBoxMode_orig,dragBoxMode_point)	
+	else:
+		dragBoxMode_orig = currentButton
+		dragBoxMode_point = currentButton
 	for x in range(len(tbutts)):
 		for y in range(len(tbutts[0])):
 			b = tbutts[x][y]

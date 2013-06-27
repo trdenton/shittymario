d67edc9709fbab04663634d00f6f9051e65cf9d9
Troy Denton <Troy.Denton@younessleeptechnologies.com>
2013-06-27 13:20:30 -0500

    refactoring this heavily, still incomplete

--- leveledit.py
+++ leveledit.py
@@ -16,177 +16,138 @@ from PIL import Image
 import pygame
 from pygame.locals import *
 
-cbutts=[]	#control buttons - the ones to select current tile
-tbutts=[]	#tile buttons - the ones that make up the map
-currentTile = 0
-dragMode = 0
-dragBoxMode=0
-currentButton = (0,0)
-dragBoxMode_orig = (0,0)
-dragBoxMode_point = (5,5)
-#pygame.init()
-
-print "loading tiles..."
-tiles = spritesheet.loadTileSheet('SMB-Tiles.png',16,16,1,1)
-class Butt:
-	def __init__(self, master, tdex=0):
-		self.x=x
-		self.y=y
-		self.tiledex = tdex
-		self.setImage(tdex)
-		self.maxdex=len(tiles)
-		self.b=Button(master,command=self.setTile())
-	def setTileDex(self, i):
-		self.tiledex=i
-
-	def setImage(self, tdex):
-		self.tiledex=tdex
-		self.image = convertImage(tiles[tdex])
-
-	def setTile(self):
-		def ret():
-			global currentTile
-			#print "setting tiledex to " + str(currentTile)
-			self.tiledex = currentTile
-			self.setImage(self.tiledex)		
-		return ret
-class TButt(Butt):
-	def __init__(self, master, tdex=0,x=0,y=0):
-		Butt.__init__(self,master,tdex)
-		self.b.bind('<Motion>',self.enterB())
-		self.x=x
-		self.y=y
-
-	def enterB(self):
-		def ret(event):
-			global dragMode
-			global dragBoxMode_point
-			global currentButton
-			currentButton = (self.x,self.y)	
-			if (dragMode):
-				self.tiledex = currentTile
-				self.setImage(self.tiledex)		
-				#print "OVER!"
-			if (dragBoxMode):
-				#print "DRAGBOX"
-				dragBoxMode_point = (self.x,self.y)
-				
-						
-		return ret
-
-def fillSquare( orig, end ):
-	global currentTile
-	global tbutts
-	for x in range(min(orig[0],end[0]),max(orig[0],end[0])+1):
-		for y in range(min(orig[1],end[1]),max(orig[1],end[1])+1):
-			
-			b = tbutts[x][y].setTile()
-			b()
-			#b.b.config(image=b.image)
-			#b.b.grid(row=b.x,column=b.y)	
-		
-
 def convertImage(im):
 	image_str = pygame.image.tostring(im, 'RGB')         # use 'RGB' to export
 	w, h      = (16, 16)
 	pic     = Image.fromstring('RGB', (w, h), image_str) # use 'RGB' to import
 	tkim = ImageTk.PhotoImage(pic)
 	return tkim
+
+class Butt:
+	def __init__(self, master, x=0,y=0,i=None):
+		self.x=x
+		self.y=y
+		self.setImage(i)
+
+	def setImage(self, i):
+		self.image = convertImage(i)
+
+
+class Tile:
+	def __init__(self, x=0,y=0,tdex=0):
+		#self.b.bind('<Motion>',self.enterB())  not applicable?
+		self.x=x
+		self.y=y
+		self.tiledex = tdex
+class App(Tk):
+	def __init__(self,parent):
+
+		Tk.__init__(self,parent)
+		self.initialize()
+	
+	def initialize(self):
+		self.cbutts=[]	#control buttons - the ones to select current tile
+		self.tiles=[]	#tile buttons - the ones that make up the map
+		#some variables to keep track of drawing state
+		self.currentTile = 0
+		self.dragMode = 0	#freeform drawing
+		self.dragBoxMode=0	#drawing a rectangle
+		self.currentButton = (0,0)
+		self.dragBoxMode_orig = (0,0)
+		self.dragBoxMode_point = (5,5)
+
+
+		#setup tk stuff
+		self.bind('<KeyPress>', self.keyPressHandler)
+		self.topFrame = Frame(self)
+		self.topFrame.pack(side=TOP)
+		self.topFrame.focus_set()
+
+		self.canvas = Canvas(self.topFrame,height="720",width="720")
+		self.tileFrame = Frame(self.canvas)
+		self.hsb = Scrollbar(self.topFrame, orient="horizontal", command=self.canvas.xview)
+
+		self.canvas.pack(side=TOP,expand=True)
+		self.hsb.pack(side=TOP,fill="y")
+		self.canvas.create_window((8,8),window=self.tileFrame,anchor="nw")
+		self.canvas.configure(xscrollcommand=self.hsb.set)
+		self.canvas.bind("<Configure>",self.resize_frame)
+
+		self.middleFrame = Frame(self,bg="red")
+		self.middleFrame.pack(side=TOP)
+
+		self.seperator = Label(self.middleFrame,text="^^MAP  VV TILES")
+		self.seperator.pack(side=LEFT)
+
+		self.bottomFrame=Frame(self)
+		self.bottomFrame.pack(side=BOTTOM)
+		self.title('Shitty Mario Level Editor')
+
+		self.tilesheet = spritesheet.loadTileSheet('SMB-Tiles.png',16,16,1,1)
+
 		
+		#setup tile structures
+		for x in range(30):
+			self.tiles.append([])
+			for y in range(30):
+				self.tiles.append(Tile(x,y))
 
+		#setup the clickable tiles
+	def drawTileButtons(self):
+		for x in range(len(self.tilesheet)):
+			c = Butt(self.bottomFrame,self.tilesheet[x])
+			c.b.config(image=c.image)#command=
+			c.b.grid(row= (x / 30),column= (x%30))
+			self.cbutts.append(c)
 
-def selectTile(x):
-	def ret():
-		global currentTile
-		currentTile=x
-		#print "current tile is " + str(currentTile)
-	return ret
+#	need to rewrite this badboy		
+#	def fillSquare( orig, end ):
+#		self.currentTile
+#		self.tbutts
+#		for x in range(min(orig[0],end[0]),max(orig[0],end[0])+1):
+#			for y in range(min(orig[1],end[1]),max(orig[1],end[1])+1):
+#				
+#				b = tbutts[x][y].setTile()
+#				b()
+#				#b.b.config(image=b.image)
+#				#b.b.grid(row=b.x,column=b.y)	
+#			
+	
+			
+	
+	
+	def keyPressHandler (self,event):
+		if (event.keysym == "space"):
+			self.dragMode=1-self.dragMode
+		if (event.keysym == "Shift_L"):
+			self.dragBoxMode = 1- self.dragBoxMode
+			if (self.dragBoxMode == 1):
+				self.dragBoxMode_orig = self.currentButton
+	
+	def resize_frame (self,e):
+		self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+	
+		
+	
+	def task(self):
+		#print dragBoxMode_orig
+		if (self.dragBoxMode):
+			fillSquare(self.dragBoxMode_orig,self.dragBoxMode_point)	
+		else:
+			self.dragBoxMode_orig = self.currentButton
+			self.dragBoxMode_point = self.currentButton
 
-def keyPressHandler (event):
-	global dragMode
-	global dragBoxMode
-	global dragBoxMode_orig
-	global dragBoxMode_point
-	global currentButton
-	if (event.keysym == "space"):
-		dragMode=1-dragMode
-	if (event.keysym == "Shift_L"):
-		dragBoxMode = 1- dragBoxMode
-		if (dragBoxMode == 1):
-			dragBoxMode_orig = currentButton
-	#print event.keysym
-def mouseUp (event):
-	mouseIsDown=0
-	#print "up"
+		self.drawTileButtons()
+		#self.drawCanvas()
 
-def resize_frame (e):
-	global canvas
-	canvas.configure(scrollregion=canvas.bbox("all"))
-
-print "done!"
-root = Tk()
-root.bind('<KeyPress>', keyPressHandler)
-#root.bind('<ButtonRelease-1>',mouseUp)
-topFrame = Frame(root)
-topFrame.pack(side=TOP)
-topFrame.focus_set()
-
-canvas = Canvas(topFrame,height="720",width="720")
-tileFrame = Frame(canvas)
-hsb = Scrollbar(topFrame, orient="horizontal", command=canvas.xview)
-
-canvas.pack(side=TOP,expand=True)
-hsb.pack(side=TOP,fill="y")
-canvas.create_window((8,8),window=tileFrame,anchor="nw")
-canvas.configure(xscrollcommand=hsb.set)
-canvas.bind("<Configure>",resize_frame)
-
-
-middleFrame = Frame(root,bg="red")
-middleFrame.pack(side=TOP)
-
-seperator = Label(middleFrame,text="^^MAP  VV TILES")
-seperator.pack(side=LEFT)
-#currentTileImage = Button(middleFrame)
-bottomFrame=Frame(root)
-bottomFrame.pack(side=BOTTOM)
-root.title('Shitty Mario Level Editor')
-
-for x in range(50):
-	tbutts.append([])
-	for y in range(30):
-		b = TButt(tileFrame,0,x,y)
-		tbutts[x].append(b)
-		tbutts[x][y].b.config(image=b.image)
-		tbutts[x][y].b.grid(row=x,column=y)
-
-for x in range(len(tiles)):
-	c = Butt(bottomFrame,x)
-	c.b.config(image=c.image,command=selectTile(x))
-	c.b.grid(row= (x / 30),column= (x%30))
-	cbutts.append(c)
+		self.after(20,self.task)
 	
 	
 
-def task():
-	global currentTile
-	global dragBoxMode_orig
-	global dragBoxMode_point
-	global dragBoxMode
-	#print dragBoxMode_orig
-	if (dragBoxMode):
-		fillSquare(dragBoxMode_orig,dragBoxMode_point)	
-	else:
-		dragBoxMode_orig = currentButton
-		dragBoxMode_point = currentButton
-	for x in range(len(tbutts)):
-		for y in range(len(tbutts[0])):
-			b = tbutts[x][y]
-			b.b.config(image=b.image)
-			b.b.grid(row=x,column=y)	
-	root.after(20,task)
+if __name__ == "__main__":
+	app = App(None)
+	app.after(20,app.task)
+	app.mainloop()
 
-root.after(20,task)
-root.mainloop()
-#just make a big scrollable grid, clicking on a tile toggles the type
+
 

3b96940f85a4380caeeff8424f559a028dcc8bcd
Troy Denton <trdenton@trdenton-N250P-N145P.(none)>
2013-06-29 19:37:27 -0500

    commiting netbook work to continue on desktop, not in working state

--- shittymario.py
+++ shittymario.py
@@ -6,6 +6,56 @@ from pygame.locals import *
 import math
 from input import Keyboard
 
+
+#pygamey-ness - have group for items, group for enemies, group for player
+#pygame is optimized to use groups - ie, an enemy sprite could use store a group of 'targets'
+#when the group is empty, bumble abount, when the group has members, pursue them.  etc.
+#associating/disassociating from groups is a very fast operation so make use of it!!
+class GameObj(pygame.sprite.Sprite):
+	def __init__(self,fs=None,r=None):
+		pygame.sprite.Sprite.__init__(self)
+		frames=fs
+		rect=r
+		animation=idleAnimation
+		#store all frames in here for every animation - have each animation just index into this
+		frames=None	#frames for animation
+
+	#override this, otherwise just first frame
+	def idleAnimation:
+		#TODO default animation	
+		while True:
+			self.image = frames[0]
+			yield None
+	
+class Player(GameObj,frames):
+	#player states
+	(PSTATE_SMALL,PSTATE_BIG,PSTATE_FIRE) = range(3)
+	#motion states
+	(MSTATE_STAND,MSTATE_WALK,MSTATE_RUN,MSTATE_JUMSTATEP,MSTATE_SWIMSTATE) = range(5)
+	def __init__(self):
+		GameObj.__init__(self,fs=frames)
+		motionState = M_STAND
+		playerState = PSTATE_SMALL
+	def walkAnimation(self):
+		while True:
+			#have 3 states to contend with - whats a pythonic swtich case?
+			self.image = (self.playerState==Player.) : ?frames[1]
+			yield None
+			self.image = frames[2]
+			yield None
+			self.image = frames[3]
+			yield None
+			self.image = frames[2]
+			yield None
+				
+class Enemy(GameObj):
+	def __init__(self):
+		GameObj.__init__(self)
+
+class Item(GameObj):
+	def __init__(self):
+		GameObj.__init__(self)
+
 class App:
 	def __init__(self):
 		#init
@@ -14,6 +64,7 @@ class App:
 		self.FPS = 30	#Frames per second
 		self.deltat = self.clock.tick(self.FPS)
 		self.screen = pygame.display.set_mode((16*40, 16*30),DOUBLEBUF)
+		pygame.display.set_caption("Shitty Mario")
 		self.keyboard = Keyboard()
 		print "Loading..."
 		#tiles = spritesheet.loadTileSheet('SMB-Tiles.png',16,16,1,1)
